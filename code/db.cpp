44d9aaa15a88f35845cb10a0855462dad43db107
ESanchezCSE <ESanchez.CSE20@gmail.com>
2020-09-03 08:05:28 -0500

    Creation of parsing function to get the working directory of the program.

--- code/db.cpp
+++ code/db.cpp
@@ -9,14 +9,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
 #include "db.h"
 
 
 int main(){
-    create_book_database();
+    std::string currentPath = get_current_dir();
+    std::cout << "Current path: " << currentPath << std::endl;
+    parse_path_from_current_directory(currentPath);
+    std::cout << "New path: " << parse_path_from_current_directory(currentPath) << std::endl;
     return 0;
 }
+
+
 /**
  * Creation of the file that will contain the data for anything about books.
  * Checks if file is
@@ -27,8 +34,9 @@ void create_book_database(){
     ofstream in_stream;
     const char *path="C:\\Users\\Edgar Sanchez\\vsCodePrograms\\rewind\\db\\bookDB.txt";
     in_stream.open(path);
-        in_stream << "File creation is successful!\n";
+        in_stream << "File creation is successful!\n";//Test code.
     in_stream.close();
+    return;
 }
 
 /**
@@ -41,4 +49,35 @@ std::string get_current_dir() {
    GetCurrentDir( buff, FILENAME_MAX );
    std::string current_working_dir(buff);
    return current_working_dir;
+}
+
+std::string parse_path_from_current_directory(std::string currentPath){
+    std::vector<std::string> result;
+    std::stringstream s_stream(currentPath); //Create string stream from the string
+
+    while(s_stream.good()){
+        std::string substr;
+        getline(s_stream, substr, '\\'); //Get first string delimited by comma
+        result.push_back(substr);
+    }
+    for(int i = 0; i < result.size(); i++){
+        std::cout << result.at(i) << std::endl;
+    }
+
+    std::string orginPath;
+    
+    for(int i = 0; i < result.size(); i++){
+        std::cout << result.at(i) << std::endl;
+        if(result.at(i).compare("rewind") != 0){
+            orginPath.append(result.at(i));
+            orginPath.append("\\");
+        }
+        else{
+            orginPath.append(result.at(i));
+            orginPath.append("\\");
+            break;
+        }
+    }
+
+    return orginPath;
 }
--- code/db.h
+++ code/db.h
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <sstream>
+
 /**
  *This is a note to myself.
  *Guard (below) makes sure that it is only included once when compiled.
@@ -9,4 +12,5 @@
 #define DB_H
 void create_book_database();
 std::string get_current_dir();
+std::string parse_path_from_current_directory(std::string currentPath);
 #endif
